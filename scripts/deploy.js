834570c0ba0514ae6bacf7d5cbeb07aa34088aba
memeboii <gonzaleznick5+GH@gmail.com>
2022-09-03 15:44:16 -0400

    simple test deploy working

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,20 +7,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  // const lockedAmount = hre.ethers.utils.parseEther("1");
 
   const CloneBase = await hre.ethers.getContractFactory("CloneContract");
   const clone = await CloneBase.deploy();
 
-  // await lock.deployed();
+  await clone.deployed();
 
-  // console.log(
-  //   `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  // );
+  console.log(
+    `clone was deployed at ${clone.address}`
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
